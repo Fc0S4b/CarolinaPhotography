e659d82989932570f12bc5b8b496c9badc3a3342
Fc0S4b <fcoj.sabugo@gmail.com>
2022-05-05 00:45:02 -0400

    add switchBtn JS

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const date = document.querySelector('#date');
+const switchBtn = document.querySelector('.switch');
+const upBtn = document.querySelector('.fa-angle-up');
+const downBtn = document.querySelector('.fa-angle-down');
+const links = document.querySelector('.links');
 
 date.innerHTML = new Date().getFullYear();
+
+switchBtn.addEventListener('click', () => {
+  links.classList.toggle('hide-links');
+  if (upBtn.classList.contains('showBtn')) {
+    upBtn.classList.remove('showBtn');
+    downBtn.classList.add('showBtn');
+  } else {
+    upBtn.classList.add('showBtn');
+    downBtn.classList.remove('showBtn');
+  }
+});
