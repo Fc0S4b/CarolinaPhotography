9a156c6d5e6a759d6e46fbabbd7a9be59b5271fd
Fc0S4b <fcoj.sabugo@gmail.com>
2022-05-05 03:11:41 -0400

    scroll setup done

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const upBtn = document.querySelector('.fa-angle-up');
 const downBtn = document.querySelector('.fa-angle-down');
 const links = document.querySelector('.links');
 
+// date
 date.innerHTML = new Date().getFullYear();
 
+// switch btn
 switchBtn.addEventListener('click', () => {
   links.classList.toggle('hide-links');
   if (upBtn.classList.contains('showBtn')) {
@@ -15,4 +17,26 @@ switchBtn.addEventListener('click', () => {
     upBtn.classList.add('showBtn');
     downBtn.classList.remove('showBtn');
   }
+});
+
+// smooth scroll
+const navbar = document.querySelector('.navbar');
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    links.classList.add('hide-links');
+
+    const id = e.target.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navbar.getBoundingClientRect().height;
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: 'smooth',
+    });
+  });
 });
